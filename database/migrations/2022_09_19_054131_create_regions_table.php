25eb1b585584ac60233e3a02c1e2ff2dd6293b0d
Ayubxon <ayubxonnematov@gmail.com>
2022-10-18 20:51:07 +0500

    asdf

--- database/migrations/2022_09_19_054131_create_regions_table.php
+++ database/migrations/2022_09_19_054131_create_regions_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->json('name');
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->timestamps();
-            $table->foreign('parent_id')->references('id')->on('regions');
         });
     }
 
