0391d88ab5d8066ab95cc71be0d6a8d514acea76
Ayubxon <ayubxonnematov@gmail.com>
2022-10-18 20:43:39 +0500

    nothing

--- database/migrations/2022_09_19_054131_create_regions_table.php
+++ database/migrations/2022_09_19_054131_create_regions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -17,11 +16,10 @@ return new class extends Migration
             $table->id();
             $table->json('name');
             $table->foreignId('parent_id')
-            ->index()
-            ->nullable()
-            ->constrained('regions')
-            ->onDelete('cascade')
-            ->onUpdate('cascade');
+                ->nullable()
+                ->constrained('regions')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->timestamps();
         });
     }
