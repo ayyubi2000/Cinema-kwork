b7242eb4e780bc6db0624a3cd3adca4a4b6e941b
Ayubxon <ayubxonnematov@gmail.com>
2022-10-31 14:26:04 +0500

    nothing

--- app/Models/ActorLatestNew.php
+++ app/Models/ActorLatestNew.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ActorLatesNew extends Model
+class ActorLatestNew extends Model
 {
     use HasFactory;
 }
